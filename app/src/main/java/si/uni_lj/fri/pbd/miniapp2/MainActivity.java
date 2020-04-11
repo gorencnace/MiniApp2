9321702c161f947f7f2dc67d6322ccf9e7fee7da
Nace Gorenc <ng0632@student.uni-lj.si>
2020-04-11 12:00:48 +0200

    processed in service and binded

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp2/MainActivity.java
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp2/MainActivity.java
@@ -3,11 +3,15 @@ package si.uni_lj.fri.pbd.miniapp2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -25,119 +29,182 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    // initializing buttons
-    private Button playButton;
-    private Button pauseButton;
-    private Button stopButton;
-    private Button gesturesOnButton;
-    private Button gesturesOffButton;
-    private Button exitButton;
+    private static final String TAG = MainActivity.class.getSimpleName();
     // Handler to update the UI
     private final Handler mUpdateHandler = new UIUpdateHandler(this);
 
     private final static int MSG_SONG_TIME = 0;
 
-    private MediaPlayer mediaPlayer;
+    private MediaPlayerService mediaPlayerService;
+    private boolean serviceBound;
 
-    // media files in assets
-    private AssetManager assetManager;
-    private String[] songs;
-    private List<String> it;
+    private TextView songTitle;
+    private TextView songDuration;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.d(TAG, "Service bound");
+
+            MediaPlayerService.RunServiceBinder binder = (MediaPlayerService.RunServiceBinder) iBinder;
+            mediaPlayerService = binder.getService();
+            serviceBound = true;
+            // add timerService.background() immediately after you finish with binding the Service
+            //mediaPlayerService.background();
+
+            // Update the UI if the service is already running the timer
+            if (mediaPlayerService.isPlaying()) {
+                updateUIPlay(mediaPlayerService.getSongId());
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.d(TAG, "Service disconnect");
+
+            serviceBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playButton = (Button) findViewById(R.id.button_play);
-        pauseButton = (Button) findViewById(R.id.button_pause);
-        stopButton = (Button) findViewById(R.id.button_stop);
-        gesturesOnButton = (Button) findViewById(R.id.button_g_on);
-        gesturesOffButton = (Button) findViewById(R.id.button_g_off);
-        exitButton = (Button) findViewById(R.id.button_exit);
+        songTitle = (TextView) findViewById(R.id.text_song_info);
+        songDuration = (TextView) findViewById(R.id.text_duration);
+    }
 
-        assetManager = getAssets();
-        try {
-            songs = assetManager.list("songs");
-        } catch (IOException e) {
-            e.printStackTrace();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent i = new Intent(this, MediaPlayerService.class);
+        i.setAction(MediaPlayerService.ACTION_START);
+        startService(i);
+        Log.d(TAG, "Starting and binding service");
+        bindService(i, mConnection, 0);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        updateUIStop();
+        if (serviceBound) {
+            unbindService(mConnection);
+            serviceBound = false;
         }
     }
 
     public void playButtonClick(View v) {
-        if (mediaPlayer == null || isPlaying()) {
-            Random rand = new Random();
-            int randN = rand.nextInt(songs.length);
-            if (mediaPlayer != null && isPlaying()) {
-                mediaPlayer.stop();
+        Log.d(TAG, "in playButtonClick");
+        if (serviceBound) {
+            Log.d(TAG, "serviceBind == true");
+            if (mediaPlayerService.isMediaPlayerNull() || mediaPlayerService.isPlaying()) {
+                Log.d(TAG, "we are getting songid");
+                int songId = mediaPlayerService.playButtonService();
+                if (songId != -1) {
+                    updateUIPlay(songId);
+                }
+            } else if (!mediaPlayerService.isPlaying()) {
+                mediaPlayerService.getMediaPlayer().start();
             }
-            // https://stackoverflow.com/questions/3289038/play-audio-file-from-the-assets-directory
-            try {
-                AssetFileDescriptor assetFileDescriptor = getAssets().openFd("songs/" + songs[randN]);
-                mediaPlayer = new MediaPlayer();
-                mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(),
-                        assetFileDescriptor.getStartOffset(),
-                        assetFileDescriptor.getLength());
-                mediaPlayer.prepare();
-                mediaPlayer.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d("MUSIC", e.toString());
-            }
-            updateUIPlay(randN);
-        } else if (!isPlaying()) {
-            mediaPlayer.start();
         }
     }
-
-    public void pauseButtonClick(View v) {
+/*
+    public int playButtonService() {
+        Random rand = new Random();
+        int randN = rand.nextInt(songs.length);
         if (mediaPlayer != null && isPlaying()) {
-            mediaPlayer.pause();
+            mediaPlayer.stop();
+        }
+        // https://stackoverflow.com/questions/3289038/play-audio-file-from-the-assets-directory
+        try {
+            AssetFileDescriptor assetFileDescriptor = getAssets().openFd("songs/" + songs[randN]);
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(),
+                    assetFileDescriptor.getStartOffset(),
+                    assetFileDescriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("MUSIC", e.toString());
+            return -1;
+        }
+        return randN;
+    }
+*/
+    public void pauseButtonClick(View v) {
+        if (serviceBound) {
+            if (!mediaPlayerService.isMediaPlayerNull() && mediaPlayerService.isPlaying()) {
+                mediaPlayerService.getMediaPlayer().pause();
+            }
         }
     }
 
     public void stopButtonClick(View v) {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer = null;
-            updateUIStop();
+        if (serviceBound) {
+            if (!mediaPlayerService.isMediaPlayerNull()) {
+                mediaPlayerService.stopButtonService();
+                updateUIStop();
+            }
         }
     }
-
+/*
+    public void stopButtonService() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+*/
     public void exitButtonClick(View v) {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            finish();
-            System.exit(0);
+        if (serviceBound) {
+            if (!mediaPlayerService.isMediaPlayerNull()) {
+                mediaPlayerService.exitButtonService();
+            }
         }
+        finish();
+        System.exit(0);
     }
 
-    public boolean isPlaying() {
+/*    public void exitButtonService() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }*/
+
+/*    public boolean isPlaying() {
+        if (mediaPlayer == null) {
+            return false;
+        }
         return mediaPlayer.isPlaying();
-    }
+    }*/
 
-    private void updateUIPlay(int n) {
-        mUpdateHandler.sendEmptyMessage(MSG_SONG_TIME);
-        TextView title = (TextView) findViewById(R.id.text_song_info);
-        title.setText(songs[n]);
+    private void updateUIPlay(int id) {
+        if (serviceBound) {
+            mUpdateHandler.sendEmptyMessage(MSG_SONG_TIME);
+            songTitle.setText(mediaPlayerService.getSongById(id));
+        }
     }
 
     private void updateUIStop() {
-        mUpdateHandler.removeMessages(MSG_SONG_TIME);
-        TextView duration = (TextView) findViewById(R.id.text_duration);
-        TextView title = (TextView) findViewById(R.id.text_song_info);
-        title.setText(R.string.song_info);
-        duration.setText(R.string.duration);
+        if (serviceBound) {
+            mUpdateHandler.removeMessages(MSG_SONG_TIME);
+            songTitle.setText(R.string.song_info);
+            songDuration.setText(R.string.duration);
+        }
     }
 
     private void updateUITimer() {
-        TextView duration = (TextView) findViewById(R.id.text_duration);
-        if (isPlaying()) {
-            duration.setText(durationToSting(mediaPlayer.getCurrentPosition()) + "/" + durationToSting(mediaPlayer.getDuration()));
+        if (serviceBound) {
+            Log.d(TAG, "update ui time");
+            if (mediaPlayerService.isPlaying()) {
+                songDuration.setText(mediaPlayerService.setTimerText());
+            }
         }
+    }
+
+/*    public String setTimerText() {
+        return durationToSting(mediaPlayer.getCurrentPosition()) + "/" + durationToSting(mediaPlayer.getDuration());
     }
 
     private String durationToSting(int d) {
@@ -149,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
         int min = d / 60;
         int sec = d % 60;
         return String.format("%d:%02d:%02d", h,  min, sec);
-    }
+    }*/
 
     static class UIUpdateHandler extends Handler {
         private final static int UPDATE_RATE_MS = 1000;
@@ -162,6 +229,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == MSG_SONG_TIME) {
+                Log.d(TAG, "update time");
                 activity.get().updateUITimer();
                 sendEmptyMessageDelayed(MSG_SONG_TIME, UPDATE_RATE_MS);
             }
--- app/src/main/java/si/uni_lj/fri/pbd/miniapp2/MediaPlayerService.java
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp2/MediaPlayerService.java
@@ -2,15 +2,143 @@ package si.uni_lj.fri.pbd.miniapp2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import java.io.IOException;
+import java.util.Random;
 
 public class MediaPlayerService extends Service {
+
+    private static final String TAG = MediaPlayerService.class.getSimpleName();
+
+    public static final String ACTION_STOP = "stop_service";
+
+    public static final String ACTION_START = "start_service";
+
+    // media files in assets
+    private AssetManager assetManager;
+    private String[] songs;
+    private MediaPlayer mediaPlayer;
+    private int songId;
+
+    // Define serviceBinder and instantiate it to RunServiceBinder
+    public class RunServiceBinder extends Binder {
+        MediaPlayerService getService() {
+            return MediaPlayerService.this;
+        }
+    }
+
+    private final IBinder serviceBinder = new RunServiceBinder();
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.d(TAG, "Binding service");
+        return this.serviceBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "Creating service");
+        assetManager = getAssets();
+        try {
+            songs = assetManager.list("songs");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "Starting service");
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Destroying service");
+    }
+
+    public int playButtonService() {
+        Random rand = new Random();
+        int randN = rand.nextInt(songs.length);
+        if (isPlaying()) {
+            mediaPlayer.stop();
+        }
+        // https://stackoverflow.com/questions/3289038/play-audio-file-from-the-assets-directory
+        try {
+            AssetFileDescriptor assetFileDescriptor = getAssets().openFd("songs/" + songs[randN]);
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(),
+                    assetFileDescriptor.getStartOffset(),
+                    assetFileDescriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            songId = randN;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("MUSIC", e.toString());
+            songId = -1;
+        }
+        return songId;
+    }
+
+    public void stopButtonService() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    public void exitButtonService() {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    public String setTimerText() {
+        return durationToSting(mediaPlayer.getCurrentPosition()) + "/" + durationToSting(mediaPlayer.getDuration());
+    }
+
+    private String durationToSting(int d) {
+        // we add 500 ms to round up
+        d += 500;
+        d = d / 1000;
+        int h = d / 3600;
+        d %= 3600;
+        int min = d / 60;
+        int sec = d % 60;
+        return String.format("%d:%02d:%02d", h,  min, sec);
+    }
+
+    public boolean isPlaying() {
+        if (mediaPlayer == null) {
+            return false;
+        }
+        return mediaPlayer.isPlaying();
+    }
+
+    public boolean isMediaPlayerNull() {
+        return mediaPlayer == null;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return this.mediaPlayer;
+    }
+
+    public String getSongById(int id) {
+        return songs[id];
+    }
+
+    public int getSongId() {
+        return songId;
     }
 }
